899fc8c7457d2c5acf3e1fc26d715aa622e1bbbb
AzubuikeOkolie <okolieazubuike@gmail.com>
2024-08-06 16:30:01 +0100

    custom responses

---
+++ HR.LeaveManagement.Application/Responses/BaseCommandResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace HR.LeaveManagement.Application.Responses
+{
+    public class BaseCommandResponse
+    {
+        public bool Success { get; set; }
+        public string Message { get; set; }
+        public List<string> Errors { get; set; }
+    }
+}
